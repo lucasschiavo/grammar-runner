517c13f0f8692e72a27f6f2acc0a7bbb6c13e733
Lucas Schiavo <lucasslongoschiavo@gmail.com>
2024-11-25 23:03:36 -0300

    update grammar rules and productions

--- GrammarRecognizer/Models/Grammar.cs
+++ GrammarRecognizer/Models/Grammar.cs
@@ -1,18 +1,32 @@
 namespace GrammarRecognizer.Models;
 
-public class Grammar(
-  string name,
-  char[] variables,
-  char[] terminals,
-  Rule[] rules,
-  char initialVariable
-)
+public class Grammar
 {
-  public string Name = name;
-  public char[] Variables = variables;
-  public char[] Terminals = terminals;
-  public Rule[] Rules = rules;
-  public char InitialVariable = initialVariable;
+  public string Name;
+  public char[] Variables;
+  public char[] Terminals;
+  public Dictionary<char, List<Production>> Productions;
+  public char InitialVariable;
+
+  public Grammar
+  (
+    string name,
+    char[] variables,
+    char[] terminals,
+    Rule[] rules,
+    char initialVariable
+  )
+  {
+    Name = name;
+    Variables = variables;
+    Terminals = terminals;
+    Productions = rules
+      .GroupBy(r => r.Variable)
+      .ToDictionary(group => group.Key, group => group.Select(kv => kv.Production).ToList());
+    InitialVariable = initialVariable;
+  }
 }
 
-public record Rule(char Variable, string Production);
+public record Rule(char Variable, Production Production);
+
+public record Production(char? Symbol, char? Variable);
--- GrammarRecognizer/Parsing/GrammarReader.cs
+++ GrammarRecognizer/Parsing/GrammarReader.cs
@@ -7,6 +7,9 @@ public partial class GrammarReader
 {
   private readonly Stack<string> _lines;
 
+  private char[] Variables = [];
+  private char[] Terminals = [];
+
   public GrammarReader(string filePath)
   {
     var lines = File.ReadLines(filePath)
@@ -26,12 +29,12 @@ public partial class GrammarReader
 
     var name = grammarDefinition[1].Value;
 
-    var variables = grammarDefinition[2].Value
+    Variables = grammarDefinition[2].Value
       .Split(",")
       .Select(v => v[0])
       .ToArray();
 
-    var terminals = grammarDefinition[3].Value
+    Terminals = grammarDefinition[3].Value
       .Split(",")
       .Select(v => v[0])
       .ToArray();
@@ -47,27 +50,68 @@ public partial class GrammarReader
       var ruleMatch = GrammarRulePattern().Match(_lines.Pop());
 
       char var = ruleMatch.Groups[1].Value[0];
-      string production = ruleMatch.Groups[2].Value;
+      string productionString = ruleMatch.Groups[2].Value;
+
+      var production = ParseProduction(productionString);
 
       rules.Add(new Rule(var, production));
     }
 
     return new Grammar(
       name,
-      variables,
-      terminals,
+      Variables,
+      Terminals,
       [.. rules],
       initialVariable
     );
   }
 
+  private Production ParseProduction(string productionString)
+  {
+    if (productionString.Length == 2)
+    {
+      char symbol = productionString[0];
+      char variable = productionString[1];
+
+      if (!Variables.Contains(variable))
+      {
+        throw new Exception($"Invalid production: {variable} is not a variable!");
+      }
+
+      if (!Terminals.Contains(symbol))
+      {
+        throw new Exception($"Invalid production: {symbol} is not a terminal!");
+      }
+
+      return new Production(symbol, variable);
+    }
+    else if (productionString.Length == 1)
+    {
+      var productionChar = productionString[0];
+
+      if (Variables.Contains(productionChar))
+      {
+        return new Production(null, productionChar);
+      }
+
+      if (Terminals.Contains(productionChar))
+      {
+        return new Production(productionChar, null);
+      }
+
+      throw new Exception($"Invalid production: {productionChar}");
+    }
+
+    return new Production(null, null);
+  }
+
   [GeneratedRegex(@"\s+")]
   private static partial Regex WhitespacePattern();
 
-  [GeneratedRegex(@"([a-zA-Z0-9_]+)=\({([a-zA-Z,]+)},{([a-zA-Z,]+)},P,([a-zA-Z])\)")]
+  [GeneratedRegex(@"([a-zA-Z0-9_]+)=\({([a-zA-Z,]+)},{([a-zA-Z0-9._\-@,]+)},P,([a-zA-Z])\)")]
   private static partial Regex GrammarTuplePattern();
 
   // valid for unitary grammars
-  [GeneratedRegex(@"([a-zA-Z]{1})->([a-zA-Z]{0,2})")]
+  [GeneratedRegex(@"([a-zA-Z]{1})->([a-zA-Z0-9._\-@]{0,2})[,]?")]
   private static partial Regex GrammarRulePattern();
 }
